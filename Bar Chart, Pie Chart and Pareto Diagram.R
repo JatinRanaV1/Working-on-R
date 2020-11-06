6bb7d6e2ffc716ed51f20d6853a4ad288e583da2
Jatin Rana <jatinrana535@gmail.com>
2020-11-06 20:46:17 +0530

    Create Bar Chart, Pie Chart and Pareto Diagram.R

---
+++ Bar Chart, Pie Chart and Pareto Diagram.R
@@ -0,0 +1,16 @@
+state <- c('New York','Texas','Hawaii','Florida','San Francisco')
+sales <- c(23327,10129,19178,13902,11723)
+df<-data.frame(state, sales)
+
+# bar chart
+barplot(df$sales, names.arg=df$state ,xlab="State" ,ylab="Sales" ,col="blue", main="Ice Cream Sales", border="black")
+
+# pie chart
+rf <- round(df$sales/sum(df$sales)*100)  # percentage or relative frequency
+lbls <- paste(df$state, rf)              # add percents to state in new object lbls
+lbls <- paste(lbls,"%",sep="")           # add % to labels
+pie(rf, lbls, main = "Ice Cream Sales", col = rainbow(length(rf)))
+
+# pareto diagram
+names(sales)<-c('New York','Texas','Hawaii','Florida','San Francisco')
+pareto.chart(sales, xlab="State", ylab="Sales")
