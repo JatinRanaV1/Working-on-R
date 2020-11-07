d24c7ecdd7f7110c6403c86f6f65bdc4017a2df9
Jatin Rana <jatinrana535@gmail.com>
2020-11-07 21:55:00 +0530

    Create Creating a Data Frame.R

---
+++ Creating a Data Frame.R
@@ -0,0 +1,10 @@
+# Enter colunms/attributes of data frame
+Width <- c(8.3, 8.6, 8.8, 10.5, 10.7, 10.8, 11, 11, 11.1, 11.2)
+Height <- c(70, 65, 63, 72, 81, 83, 66, 75, 80, 75)
+Volume <- c(10.3, 10.3, 10.2, 16.4, 18.8, 19.7, 15.6, 18.2, 22.6, 19.9)
+
+# Create data frame
+df<-data.frame(Width, Height, Volume)
+
+# Print the fata frame
+print(df)
