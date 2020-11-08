d7b97cd3bb27b27322a437a83467a01d9bccf718
Jatin Rana <jatinrana535@gmail.com>
2020-11-08 23:16:23 +0530

    Update Bar Chart, Pie Chart and Pareto Diagram.R

--- Bar Chart, Pie Chart and Pareto Diagram.R
+++ Bar Chart, Pie Chart and Pareto Diagram.R
@@ -11,7 +11,7 @@ lbls <- paste(df$state, rf)              # add percents to state in new object l
 lbls <- paste(lbls,"%",sep="")           # add % to labels
 pie(rf, lbls, main = "Ice Cream Sales", col = rainbow(length(rf)))
 
-# install and import 'gcc' library first
+# install and import 'qcc' library first
 # pareto diagram
 names(sales)<-c('New York','Texas','Hawaii','Florida','San Francisco')
 pareto.chart(sales, xlab="State", ylab="Sales")
